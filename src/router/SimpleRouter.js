9e7705b73096070cb6b0c5c0fe6fb9d911850f30
carolinac <carol.dev.ve@gmail.com>
2024-05-05 17:37:21 -0400

    add register, login and save user in local

--- src/router/SimpleRouter.js
+++ src/router/SimpleRouter.js
@@ -1,11 +1,43 @@
 import React from "react";
-import { useAuth } from "../context/AuthContext";
 import LoginForm from "../pages/auth/LoginScreen";
 import LoadCSV from "../components/LoadCSV";
+import {BrowserRouter, createBrowserRouter , Route,  Routes } from 'react-router-dom';
+import ProtectedRoute from "./ProtectedRoute";
+import RegisterForm from "../pages/auth/RegisterScreen";
 
-const SimpleRouter = () => {
-  const { user } = useAuth();
-  return user?.email ? <LoadCSV /> : <LoginForm />;
-};
+
+// const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <LoginForm/>
+//     },
+//     {
+//         path: "/register",
+//         element: <RegisterForm/>,
+//       },
+//       {
+//         path: "/data",
+//         element: <ProtectedRoute/>,
+//       },
+//   ]);
+
+function SimpleRouter() {
+  return (
+
+    <BrowserRouter>
+    <Routes>
+        <Route path="/data" element={
+        <ProtectedRoute>
+            <LoadCSV/>
+        </ProtectedRoute>
+     }/>
+        <Route path="/" element={<LoginForm/>} />
+        <Route path="/register" element={<RegisterForm/>} />
+
+    </Routes>
+    </BrowserRouter>
+
+  );
+}
 
 export default SimpleRouter;
