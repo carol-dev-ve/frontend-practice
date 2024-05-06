48cd2aefcdc6f0378ad31660d517f08940c92444
carolinac <carol.dev.ve@gmail.com>
2024-05-05 21:17:02 -0400

    improve register to redirect

--- src/pages/auth/RegisterScreen.js
+++ src/pages/auth/RegisterScreen.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { redirect, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 const RegisterForm = ({ onClose }) => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState(null);
 
+  const handleUserName = (e) => {
+    setUsername(e.target.value);
+  };
+  
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
@@ -24,15 +32,34 @@ const RegisterForm = ({ onClose }) => {
       setError('Passwords do not match');
       return;
     }
+    if(!email || !username || !password || !confirmPassword) {
+      toast.warning(
+        `Please enter all data`,
+        {
+          position: "top-right",
+          autoClose: 5000,
+          toastId: new Date().toString(),
+        }
+      );
+      return
+    }
     try {
-      // Make a request to your registration endpoint
-    //   const response = await AxiosHuggi.post('/register', { email, password });
-      // Handle successful registration
-    //   console.log('Registration successful:', response.data);
-      // Reset form fields after successful registration
+      toast.success(
+        `Register. Please go to login with credentials provide in readme`,
+        {
+          position: "top-right",
+          autoClose: 5000,
+          toastId: new Date().toString(),
+        }
+      );
       setEmail('');
       setPassword('');
       setConfirmPassword('');
+      setTimeout(() => {
+        return navigate('/')
+      }, 3000);
+  
+
     } catch (error) {
       // Handle registration error
       setError(error.message);
@@ -75,6 +102,16 @@ const RegisterForm = ({ onClose }) => {
             />
           </div>
           <div className="mb-4">
+            <label htmlFor="username" className="block text-sm font-semibold text-gray-600 mb-1">Username</label>
+            <input
+              id="username"
+              className="border border-gray-300 rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-500"
+              value={username}
+              onChange={handleUserName}
+              required
+            />
+          </div>
+          <div className="mb-4">
             <label htmlFor="password" className="block text-sm font-semibold text-gray-600 mb-1">Password</label>
             <input
               type="password"
@@ -109,6 +146,7 @@ const RegisterForm = ({ onClose }) => {
           <a className="text-blue-500 hover:underline" href="/">Login here</a>
         </div>
       </div>
+      <ToastContainer limit={2} />
     </div>
   );
 };
